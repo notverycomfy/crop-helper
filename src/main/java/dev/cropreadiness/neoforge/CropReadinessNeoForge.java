adf834debf8cbf63ecb5f32663b282dffcfd9b13
notverycomfy <Cfrazier9452@gmail.com>
2026-07-23 23:03:12 -0700

    Standardize development workflow and code style

--- src/main/java/dev/cropreadiness/neoforge/CropReadinessNeoForge.java
+++ src/main/java/dev/cropreadiness/neoforge/CropReadinessNeoForge.java
@@ -1,10 +1,13 @@
 package dev.cropreadiness.neoforge;
 
 import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(CropReadinessNeoForge.MOD_ID)
 public final class CropReadinessNeoForge {
     public static final String MOD_ID = "crop_helper";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public CropReadinessNeoForge() {
     }
