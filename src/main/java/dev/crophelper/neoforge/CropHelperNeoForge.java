ddbb5d93017aeb219a4cddda7a8085fc695d12a4
Comfy <notverycomfy@users.noreply.github.com>
2026-07-24 20:41:16 -0700

    Updated Crop Helper project names

--- src/main/java/dev/crophelper/neoforge/CropHelperNeoForge.java
+++ src/main/java/dev/crophelper/neoforge/CropHelperNeoForge.java
@@ -1,14 +1,14 @@
-package dev.cropreadiness.neoforge;
+package dev.crophelper.neoforge;
 
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(CropReadinessNeoForge.MOD_ID)
-public final class CropReadinessNeoForge {
+@Mod(CropHelperNeoForge.MOD_ID)
+public final class CropHelperNeoForge {
     public static final String MOD_ID = "crop_helper";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public CropReadinessNeoForge() {
+    public CropHelperNeoForge() {
     }
 }
